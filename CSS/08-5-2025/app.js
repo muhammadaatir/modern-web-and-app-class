75424e3c3d345252049741244df26f5fbaa239a9
muhammadaatir <aatirmemon.8211@gmail.com>
2025-05-10 11:01:37 -0700

    Code updated.

---
+++ CSS/08-5-2025/app.js
@@ -0,0 +1,15 @@
+var myName = "test";
+var number = 12343;
+var test = true;
+
+
+console.log(myName,1)
+console.log(myName,2)
+console.log(myName,3)
+console.log(myName,4)
+
+
+console.log("test",1)
+console.log("test",2)
+console.log("test",3)
+console.log("test",4)
---
+++ JS/js_code/app.js
@@ -0,0 +1,35 @@
+// 10-05-2025
+// var test = "12345";
+// test = "testing"; // reassign
+
+// var test2;// initialization
+// test2 = true; //assign
+// console.log(test2);
+
+// var test2 = "SMIT" ; //redeclaration
+// console.log(test2);
+// var test = 0;   
+// console.log(test);
+// var my_name = 'Muhummad Aatir';
+// var num = 12345;
+// var boolean = true;
+
+// console.log(my_name, num, boolean, 1245);
+
+// document.write(my_name)
+
+// alert(1234);
+
+// var num = "Muhammad";
+// var num2 = "Aatir";
+// console.log(num + num2);
+
+// var num = 12;
+// var num2 = 10;
+// alert(num + num2);
+
+var num = +"10";
+var test = true
+var str = "12";
+
+console.log(typeof test); 
