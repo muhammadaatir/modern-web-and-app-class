a0d5dc7b8a8bbe4d107edce23b4f7c1659a3de92
muhammadaatir <aatirmemon.8211@gmail.com>
2025-05-17 09:26:58 -0700

    Code of 13 and 15 may.

--- JS/js_code/app.js
+++ JS/js_code/app.js
@@ -28,8 +28,97 @@
 // var num2 = 10;
 // alert(num + num2);
 
-var num = +"10";
-var test = true
-var str = "12";
+// var num = +"10";
+// var test = true
+// var str = "12";
 
-console.log(typeof test); 
+// console.log(typeof test); 
+
+// 13-05-2025
+
+// var num = "12";
+// var num2 = 5;
+// var result = "12" - "17"
+// console.log(typeof result)
+// console.log(num + num2);
+// console.log(num - num2);
+// console.log(num * num2);
+// console.log(num / num2);
+// console.log(num % 2);
+
+// var firstName = "Muhammad  ";
+// var lastName = " Aatir ";
+// var test = "test";
+
+// console.log(firstName + lastName + test)
+
+// var num = 2;
+// ++num; //pre increament
+// num++; // post increament
+
+// console.log(num++);
+// console.log(--num);
+
+// console.log(++num);
+
+// console.log(num + 1)
+
+// num++; // num = num + 1
+// console.log(++num)
+// console.log(num);
+
+// var number = 10
+// number++ or ++number --> number = number + 1
+// console.log(number--);
+// console.log(number);
+
+// 15-05-2025
+
+// var str1 = "Muhammad";
+// var str2 = "Aatir";
+// var str3 = "Aatir";
+// var space = ", ";
+// var my_name_test_test = str1 + space + str2 + space + str3;
+
+// console.log(my_name_test_test);
+// console.log(my_name_test_test);
+
+// // alert(my_name_test_test);
+
+// var a = 450;
+// var b = 500;
+
+// a = a + b; //900
+
+// b = a - b; //900 - 500 = 400
+// a = a-b; 
+
+// console.log(a, b);
+// console.log("400", 400, true) // static
+
+// console.log((1 + 4) * 5);
+
+// var number = 4;
+// number = number - 1;
+// number--;
+// console.log(--number)
+// console.log(number = "class");
+
+// console.log(number)
+// var question = "Enter your name"
+// var user_name = prompt(question)
+
+// alert("Hello" + " " + user_name)
+
+// var num1 = Number(prompt("Enter first number"))
+// var num2 = Number(prompt("Enter second number"))
+
+// var sum = num1 * num2
+
+// alert("Your sum is " + sum)
+
+// 17-05-2025
+
+var message = "Enter your name"
+
+prompt(message + 12345)
