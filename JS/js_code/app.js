0610bc390d90309f908e4edbf58f187bb539f987
ALIKHAN4 <aatirmemon.8211@gmail.com>
2025-05-24 11:02:30 -0700

    Class code updated

--- JS/js_code/app.js
+++ JS/js_code/app.js
@@ -16,7 +16,8 @@
 
 // console.log(my_name, num, boolean, 1245);
 
-// document.write(my_name)
+// document.write("my_name <br>")
+// document.write("my_name")
 
 // alert(1234);
 
@@ -119,6 +120,187 @@
 
 // 17-05-2025
 
-var message = "Enter your name"
+// var message = "Enter a number, your number will be doubled"
 
-prompt(message + 12345)
+// var user_number = +prompt(message);
+// console.log(typeof user_number);
+
+// var result = user_number + 2;
+// alert("Your name is " + result);
+
+// 20-05-2025
+//"abc" ==> (Boolean) true.
+
+// "false" (boolean)
+// var user_name = prompt("Enter your name");
+// var num = prompt("Enter a number");
+// if (num <= 5) {
+//     alert("You won")
+// } else {
+//     alert("You loss")
+// }
+// if (num < 5) {
+//     alert("your number is less than 5")
+// } else if (num === 5) {
+//     alert("your number is equal to 5")
+// } else {
+//     alert("your number is greater than 5")
+
+// }
+
+
+// var num1 = +prompt("Enter a number");
+// var operator = prompt("What operation you want to perform(+,-,/,*)");
+// var num2 = +prompt("Enter second number");
+
+// if(operator == "+") {
+//     alert(num1 + num2)
+// }else if(operator == "-") {
+//     alert(num1 - num2)
+// }else if(operator == "/") {
+//     alert(num1 / num2)
+// }else if(operator == "*") {
+//     alert(num1 * num2)
+// } else {
+//     alert("Enter a valid operator (+,-,*,/)")
+// }
+
+// var num1 = +prompt("Enter a number");
+// var result = num1 % 2
+// if( result === 0) {
+//     alert("Even Number")
+// } else {
+//     alert("Odd Number")
+// }
+
+// 22-05-2025
+// var user_number = Number(prompt("Enter your age"))
+// var number = 3;
+// if (user_number > number) {
+//     alert("Your number is greater")
+// } else if (number === user_number) {
+//     alert("Your number is same")
+// } else if (user_number < number) {
+//     alert("Your number is lesser")
+// } else {
+//     alert("Enter a valid number")
+// }
+
+// if (user_number < 5 && user_number >= 1 && user_number === 4) {
+//     alert("SMIT")
+// }
+
+// if(user_number < 50000) {
+//     alert("Tax is not applicable")
+// } else if (user_number >= 50000 && user_number < 100000) {
+//     var tax = user_number * 0.05
+//     alert("Your tax is " + tax)
+// } else if (user_number >= 100000 && user_number < 150000) {
+//     var tax = user_number * 0.10
+//     alert("Your tax is " + tax)
+// } else if (user_number >= 150000 && user_number < 200000) {
+//     var tax = user_number * 0.15
+//     alert("Your tax is " + tax)
+// } else if (user_number >= 200000) {
+//     var tax = user_number * 0.20
+//     alert("Your tax is " + tax)
+// } else {
+//     alert("Enter a valid number")
+// }
+//false               ||            //true
+//false             //true              //false
+// if (user_number < 21 || (user_number > 65 || user_number === 25)) {
+//     alert("Age group matches")
+// } else {
+//     alert("wrong COndition")
+// }
+
+
+// var a;
+// var b = 5;
+// var c = 10;
+
+
+// var user_number1 = Number(prompt("Enter number 1"))
+// var user_number2 = Number(prompt("Enter number 2"))
+// // var user_number3 = Number(prompt("Enter number 3"))
+
+// if (user_number1 > 10) {
+//     alert("your number is greater than 10")
+// } else if (user_number1 < 10) {
+//     if (user_number2 > 6) {
+//         alert("your number is greater than 6")
+//     } else if (user_number2 === 6) {
+//         alert("your number is 6")
+//     } else {
+//         alert("your number is lesser than 6")
+//     }
+//     alert("your number is lesser than 10")
+// } else {
+//     alert("test")
+// }
+// var number = +prompt("enter a number");
+
+// if (number === 10) {
+//     alert("SMIT")
+// }
+// else if (number < 10) {
+// alert("less")
+// }
+// else if (number > 10) {
+// alert("greater")
+// }
+// else {
+//     alert("invalid number")
+// }
+
+// if (number > 10 && number < 100) {
+//     console.log("correct number");
+// } else if ((number >= 7 && number < 11) || (number < 105 && number > 99)) {
+//     console.log("you are close");
+// } else {
+//     console.log("wrong");    
+// }
+
+// if (number) {
+//     if (number > 10) {
+//         console.log("OKay");
+//     } else {
+//         console.log("NOt OKay");
+//     }
+//     console.log("Your number is true");
+// } else {
+//     console.log("Your number is not true");
+    
+// }
+
+// if (number && number > 10) {
+//     console.log("Test");
+// }
+
+// var roll_number = [12, 15, 40, 50, 65];
+// var student_names = ["ali", "ahmed", "tahir"]
+// console.log(roll_number.length, student_names[2]);
+
+// roll_number[5] = 45 
+
+// console.log(roll_number);
+
+// roll_number.push(400)
+// roll_number.pop()
+// student_names.shift()
+// student_names.unshift("aatir")
+// console.log(student_names);
+
+// var numbers = [1, 5, 6, 7, 8, 56, 34]
+
+// var strings = ["SMIT", "Saylani", "Batch 16", "Testing", "HTML", "CSS"]
+
+// numbers[8] = 7
+// console.log(numbers[numbers.length - 1]);
+// console.log(numbers[8]);
+
+// strings.unshift()
+// console.log(strings);
+
+ 
