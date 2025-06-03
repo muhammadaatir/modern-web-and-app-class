cf9c57bd6d8126d304dbe5341bffd44c853252b5
ALIKHAN4 <aatirmemon.8211@gmail.com>
2025-06-03 11:13:31 -0700

    Class code updated

--- JS/js_code/app.js
+++ JS/js_code/app.js
@@ -271,7 +271,7 @@
 //     console.log("Your number is true");
 // } else {
 //     console.log("Your number is not true");
-    
+
 // }
 
 // if (number && number > 10) {
@@ -302,5 +302,253 @@
 
 // strings.unshift()
 // console.log(strings);
+// var str = 234;
 
- 
+// var arr = [1, 2.3, 4, 7, 8, 9, 1011] 
+// var arr2 = arr;
+// console.log(arr,arr2);
+
+// console.log(arr.length);
+
+// 4       -   1
+// console.log(arr[arr.length - 1]); //arr[3]
+
+// console.log(arr[arr.length]);
+
+// console.log(arr.push(123,345));
+// console.log(arr);
+// console.log(arr.pop());
+// console.log(arr);
+// console.log(arr.unshift(123));
+// console.log(arr);
+// arr.shift();
+// console.log(arr.shift());
+// console.log(arr);
+// var copy = arr.slice(2, arr.length)
+// console.log(copy)
+
+
+// var array = ["Ali", "Ahmed", "Tahir", "zeeshan", "owais"];
+// index number to start operation(add, remve,replace), hw many elements to remove, values to add in array
+// array.splice(2,0,"zeeshan", "owais") add through splice
+// array.splice(1,2,"zeeshan", "owais") splice for replace
+// array.splice(0, 1)
+// console.log(
+
+//     array.splice(1, array.length - 3)
+// );
+// console.log(array);
+
+// var numArray = [4, 6, 103, 76, 99, 16, 48];
+
+// console.log(  
+//     numArray.splice(3,0, 10, 50)
+// );
+// numArray.splice(4,2, 87, 98, 76,545 )
+// numArray.splice(1,3)
+// numArray.splice(numArray.length - 5, 5)
+// numArray.splice(1, numArray.length - 4)
+// console.log(numArray);
+
+// var str = prompt("Enter a alphabet")
+
+// if(str.charCodeAt() >= 97 &&  str.charCodeAt() <= 122) {
+//     console.log("Smaller case");
+// } else if (str.charCodeAt() >= 65 && str.charCodeAt() <= 90) {
+//     console.log("Upper case");
+// }
+
+// var arr = [1, 3, 7, 8, 5, 19, 476];
+
+// arr.splice(3, 0, 13, 67)
+// arr.splice(4, 1)
+// arr.splice(2, 2, 43, 56, 78)
+// arr.splice(3, arr.length - 4)
+// var new_arr = arr.slice(2, 6)
+// console.log(new_arr, arr);
+
+// var number = +prompt("Enter a number 0-100")
+
+// var correct_num = [5, 20, 36, 40, 99]
+
+// if (number === correct_num[0]) {
+//     alert("You won")
+// } else {
+//     alert("You lose")
+// }
+// var i;
+//    0     0<5 1<5 2<5...5<5
+// for (i = 0; i < 5; i++) {    
+//     if(number === correct_num[i]){
+//         alert("You won")
+//     } else {
+//         if(i === 4) {
+//             alert("you lose");
+//         }
+//     }
+// }
+//2 * 1 = 2
+// console.log("ok");
+// for(var i = 1; i < 11; i++) {
+// console.log("2 * " + i + " = " + i*2 )
+// console.log("12 * " + i + " = " + i*12); 
+// }
+// var number = +prompt("Enter a number 0-100")
+
+// var correct_num = [5, 20, 36, 40, 99]
+// for (var i = 0; i < 5; i++) {
+//     var flag = false
+//     console.log(i);
+
+//     if (number === correct_num[i]) {
+//         flag = true
+//         alert("You won")
+//     }
+//     if (number > 100) {
+//         break;
+//     }
+// }
+
+// if (!flag) {
+//     alert("You loss")
+// }
+
+// 31-05-2025
+// var arr = [1, 3, 5, 6, 7, 8, 9]
+// for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 !== 0) {
+//         console.log(arr[i]);
+//     } else {
+//         console.log(arr[i]  + " is not a odd number")
+//     }   
+// }
+
+// var even = 0;
+// var odd = 0;
+// var isTenFound = false;
+// var arr = [1, 3, 5, 7, 10, 8, 90]
+// for (var i = 0; i < arr.length; i++) {
+    // if (arr[i] % 2 !== 0) {
+    //     console.log(arr[i] + " is an odd number");
+    //     odd++
+    // } else {
+    //     even++
+    //     console.log(arr[i] + " is not a odd number")
+    // }
+// if(arr[i] > even ) {
+//     even = arr[i]
+// }
+    // if (arr[i] === 10) {
+        // isTenFound = true;
+    // }
+// }
+// console.log(even);
+
+// if (isTenFound) {
+//     console.log("array has 10 number");
+
+// }
+// console.log("your array has " + odd + " odd numbers");
+// console.log("your array has " + even + " even numbers");
+
+// var number = +prompt("Enter a number(1-100)");
+// var correct_numbers = [1,6,80,99]
+// for (var i = 0; i < correct_numbers.length; i++) {
+//     // if (number > 100 || number < 0) {
+//     //     break
+//     // }
+//     if (correct_numbers[i] === number) {
+//         console.log("Correct");
+//     } else {
+//         console.log("incorrect")
+//     }   
+//     break;
+// }
+
+// var first_name = ["Ali", "Ahmed"];
+// var last_name = ["Tahir", "Ahsan"]
+// for (var i = 0; i < first_name.length; i++) {
+//     alert("hello world")
+//     for (var j = 0; j < last_name.length; j++){
+//         console.log(first_name[i]+ " " + last_name[i]);
+
+//     }
+// }
+// var arr = [[1, 4, 5], [2, 5, 7], ["a", "b", "c"]]
+// for (var i = 0; i < arr.length; i++) {
+//     // alert("hello world")
+//     // console.log(
+//     //     arr[i]
+//     // );
+//     // i = 0
+//     // console.log(arr[i]);
+
+//     for (var j = 0; j < arr[i].length; j++) {
+//         // console.log(arr[i][j]);
+//         if (arr[i][j] % 2 == 0) {
+//             console.log(arr[i][j]);
+//         } else {
+//             console.log(arr[i][j] + " is not a odd number")
+//         }
+//     };
+// }
+// false
+// undefined
+// null
+// ""
+// NaN
+
+// var arr = ["apple", "mango", "grapes"]
+
+// console.log(
+//     arr.join()
+// );
+
+// var str = "apple, mango, grapes"
+// console.log(str.split(" "));
+
+// var number = [3, 6, 7, 1, 100, 10, 76]
+// var arr = ["apple", "mango", "grapes", "banana"]
+// console.log(number.sort((a, b) => b-a));
+
+// 3-06-2025
+//Revision Classes
+
+// console.log("SMIT");
+// cile.log("ee");
+// console.log("Saylani");
+
+// var fruitNames = 'Apple Mango Grapes';
+// var number = 12233.567;
+// var my_var = true; 
+
+// console.log(fruitNames, number, my_var, "hello world", 132345456, true, null);
+// var my_name; //declaration
+
+// my_name = "Aatir"; //assignment
+
+// my_name="Ali"; //reassignment
+
+// var My_name = "Ahmed"; // redeclaration
+
+// console.log(8/2);
+
+// alert("Hello World", );
+
+// var user_name = Boolean(prompt("Enter a number"));
+// // var result = user_name + 10;
+// console.log(user_name);
+
+// alert("Double of your number " + result);
+
+// var paragraph = 'Lorem ipsum ut laborficia `Aatir` mollit anim id est laborum.';
+
+// var num1 = 12;
+// var num2 = 3;
+
+// num1 = num1 + num2; //sum storing in num1
+//        //sum - num2 =num1
+// num2 = num1 - num2;
+//     //sum - num1 = num2
+// num1 = num1-num2;
+// console.log(num1, num2);
